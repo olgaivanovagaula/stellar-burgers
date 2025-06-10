ca6f7d2ccbdc0abad03a552fa2ccb976179bcaaf
olgaivanovagaula <olgaivanova2512@yandex.ru>
2025-06-10 14:11:31 +0300

    userReducer

--- src/services/slices/userApi.ts
+++ src/services/slices/userApi.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { registerUserApi, TRegisterData } from '@api';
-import { setCookie } from '../../utils/cookie';
+import { registerUserApi, TRegisterData, TLoginData, loginUserApi, logoutApi, updateUserApi,  getUserApi,forgotPasswordApi } from '@api';
+import { setCookie, deleteCookie } from '../../utils/cookie';
 
 export const registerUser = createAsyncThunk(
   'user/registerUser',
@@ -10,3 +10,45 @@ export const registerUser = createAsyncThunk(
     return res;
   }
 );
+
+
+export const loginUser = createAsyncThunk (
+    'user/loginUser',
+    async (data: TLoginData) => {
+      const res = await loginUserApi(data )
+      setCookie('refreshToken', res.refreshToken)
+      localStorage.setItem('email', res.user.email)
+      localStorage.setItem("accessToken", res.accessToken )
+      return res
+    }
+)
+
+export const logoutUser = createAsyncThunk(
+  'user/logoutUser',
+  async () => {
+    localStorage.removeItem("accessToken")
+    deleteCookie("refreshToken")
+    const res = await logoutApi()
+    localStorage.removeItem("email")
+    return res
+  }
+)
+
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+   async (data: TRegisterData) => {
+    const res = await updateUserApi(data)
+    return res
+   }
+)
+
+export const forgotUserPass = createAsyncThunk(
+    'user/fogotUserPass',
+    async (data: {email: string}) => {
+      const res = await forgotPasswordApi(data);
+      return res
+
+    }
+)
+
+export const getUser = createAsyncThunk("user/getUser", getUserApi)
--- src/services/slices/userReducer.ts
+++ src/services/slices/userReducer.ts
@@ -1,19 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { registerUser } from './userApi';
+import { registerUser, loginUser, logoutUser, updateUser, forgotUserPass, getUser } from './userApi';
 import { TRegisterData } from '@api';
+import { TUser } from '@utils-types';
 
 type TypeInitialState = {
-  userData: TRegisterData | null;
+  userData: TUser | null;
   isLoading: boolean;
   registerError: string | null;
   loginError: string | null;
+  authChecked: boolean
+  logoutError: string | null;
+  updatedError: string | null;
+  forgotError: string | null;
+  checkAuthError: string | null;
+  isAuthentificatied: boolean;
 };
 
 const initialState: TypeInitialState = {
+  authChecked: false,
   userData: null,
   isLoading: false,
   registerError: null,
-  loginError: null
+  loginError: null,
+  logoutError: null,
+  updatedError: null,
+  forgotError: null,
+  checkAuthError: null,
+  isAuthentificatied: false
 };
 
 const userApiSlice = createSlice({
@@ -32,11 +45,94 @@ const userApiSlice = createSlice({
       .addCase(registerUser.rejected, (state, action) => {
         state.isLoading = false;
         state.registerError = action.error.message ?? 'Ошибка при регистрации';
+        state.authChecked = true
       })
       .addCase(registerUser.fulfilled, (state, action) => {
         state.isLoading = false;
         state.registerError = null;
-      });
+        state.isAuthentificatied = true
+        state.userData = action.payload.user
+      })
+
+
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.loginError = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.authChecked = false;
+        state.isLoading = false;
+        state.loginError = action.error.message ?? 'Ошибка при входе';
+        
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.authChecked = true;
+        state.isLoading = false;
+        state.loginError = null;
+        state.isAuthentificatied = true;
+        state.userData = action.payload.user;
+      })
+
+
+      .addCase(logoutUser.pending, (state) => {
+        state.isLoading = true;
+        state.logoutError = null;
+      })
+      .addCase(logoutUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.logoutError = action.error.message ?? 'Ошибка при выходе';
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        state.authChecked = false
+        state.isLoading = false;
+        state.logoutError = null;
+        state.userData = null
+      })
+
+
+       .addCase(updateUser.pending, (state) => {
+        state.isLoading = true;
+        state.updatedError = null;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.updatedError = action.error.message ?? 'Ошибка при выходе';
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.updatedError = null;
+        state.userData = action.payload.user
+      })
+
+
+       .addCase(forgotUserPass.pending, (state) => {
+        state.isLoading = true;
+        state.forgotError = null;
+      })
+      .addCase(forgotUserPass.rejected, (state, action) => {
+        state.isLoading = false;
+        state.forgotError = action.error.message ?? 'Ошибка при смене пароля';
+      })
+      .addCase(forgotUserPass.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.forgotError = null;
+      })
+
+
+      .addCase(getUser.pending, (state) => {
+        state.isLoading = true;
+        state.checkAuthError = null;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.checkAuthError = action.error.message ?? 'Ошибка при входе';
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.checkAuthError = null;
+        state.userData = action.payload.user
+      })
+      
   }
 });
 
